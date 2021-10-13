2636e2506142648186bd9928b1eb0160b5fae14b
Ted Satcher <ted.satcher@gmail.com>
2021-10-13 07:00:17 -0500

    Adding interactive skeleton

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
-import Data.List
+import Data.Char (toLower)
+import System.Exit (exitSuccess)
+import Control.Monad
 
 {--
   Here we have an implementation of the game BattleShip in Haskell.
@@ -21,7 +23,11 @@ boardHeader :: String
 boardHeader = " 0 1 2 3 4 5 6 7 8 9"
 
 -- Draw the board
-renderBoard :: Bool -> Hits -> Misses -> Ships -> String
+renderBoard :: Bool -- ^ False turns off ship rendering
+            -> Hits -- ^ The list of hit coordinates
+            -> Misses -- ^ The list of miss coordinates
+            -> Ships -- ^ The list of ship coordinates
+            -> String
 renderBoard = undefined
 
 -- Render the board with ships hidden
@@ -33,8 +39,22 @@ postGameBoard :: Hits -> Misses -> Ships -> String
 postGameBoard = renderBoard False
 
 
+splash :: String
+splash = "\n\nWelcome to Haskell BattleShip!\n\nCommand options are (n)ew or (q)uit"
+
+play :: IO ()
+play = putStrLn "Playing..."
+
+quit :: IO ()
+quit = do
+  putStrLn "\n\nThanks for playing =)"
+  exitSuccess
+
+
 main :: IO ()
-main = do
-  let coord = Coord (1, 2)
-      coords = [coord, Coord (3, 4)]
-  putStrLn $ intercalate "\n" $ show <$> coords
+main = forever $ do
+         putStrLn splash
+         cmd@(c:_) <- getLine
+         if 'n' == (toLower c) then play
+         else if 'q' == (toLower c) then quit
+         else putStrLn $ cmd <> " isn't a valid command."
